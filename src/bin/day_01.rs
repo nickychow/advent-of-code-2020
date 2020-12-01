7c18e4aec92c6d77f1c56a7fa68662ee54863b13
Nick Chow <nicky.chow@gmail.com>
2020-12-01 23:32:56 +1000

    finish day 01 part 1

--- src/bin/day_01.rs
+++ src/bin/day_01.rs
@@ -1,1 +1,23 @@
+use env_logger;
+use std::collections::HashSet;
 
+use aoc2020::*;
+
+fn main() {
+    env_logger::init();
+
+    let input = read_input("./data/day_01/input");
+    log::info!("{:#?}", input);
+    println!("Part 1: {:?}", part_01(&input))
+}
+
+fn part_01(input: &HashSet<u64>) -> u64 {
+    for x in input {
+        let y = 2020 - x;
+        if input.contains(&y) {
+            return x * y;
+        }
+    }
+
+    0
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,22 @@
 // missing_docs, missing_debug_implementations
 #![warn(rust_2018_idioms)]
 use std::collections::HashSet;
-use std::fs::File;
-use std::io::{self, BufRead};
+use std::fmt::Debug;
+use std::hash::Hash;
 use std::path::Path;
+use std::str::FromStr;
 
-pub type Result<T> = std::result::Result<T, anyhow::Error>;
-
-pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+pub fn read_input<P, T>(file_name: P) -> HashSet<T>
 where
     P: AsRef<Path>,
+    T: FromStr + Hash + Eq + Debug,
+    <T as FromStr>::Err: Debug,
 {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
-
-pub fn read_hashset<P>(filename: P) -> Result<HashSet<u64>>
-where
-    P: AsRef<Path>,
-{
+    std::fs::read_to_string(file_name)
+        .expect("file not found!")
+        .lines()
+        .map(|l| l.trim())
+        .filter(|l| !l.is_empty())
+        .map(|l| l.parse().unwrap())
+        .collect()
 }
